da94d5206dbf32bb46e9b9be382089e92ffb2dbc
scott2srikanth <srikanthreddy.com@gmail.com>
2021-07-29 20:32:07 +0530

    SharedPrefrences

--- app/src/main/java/com/example/firebasedemoproject/LanguageAvtivity.kt
+++ app/src/main/java/com/example/firebasedemoproject/LanguageAvtivity.kt
@@ -1,16 +1,24 @@
 package com.example.firebasedemoproject
 
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.core.Context
 import java.util.*
 
 class LanguageAvtivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        loadLanguage()
+
         setContentView(R.layout.activity_language_avtivity)
 
         var langList = arrayOf("English","हिंदी", "తెలుగు")
@@ -23,35 +31,15 @@ class LanguageAvtivity : AppCompatActivity() {
             alertBox.setSingleChoiceItems(langList,-1){
                 dialog, zz ->
                 if (zz == 0){
-                    var locale = Locale("en")
-                    Locale.setDefault(locale)
-
-                    val congif = Configuration()
-                    congif.locale = locale
-                    baseContext.resources.updateConfiguration(congif,baseContext.resources.displayMetrics)
-
+                    setLocale("en")
                     recreate()
                 }
                 else if (zz == 1){
-                    var locale = Locale("hi")
-                    Locale.setDefault(locale)
-
-                    val congif = Configuration()
-                    congif.locale = locale
-                    baseContext.resources.updateConfiguration(congif,baseContext.resources.displayMetrics)
-
-
-
+                    setLocale("hi")
                     recreate()
                 }
                 else if (zz == 2){
-                    var locale = Locale("te")
-                    Locale.setDefault(locale)
-
-                    val congif = Configuration()
-                    congif.locale = locale
-                    baseContext.resources.updateConfiguration(congif,baseContext.resources.displayMetrics)
-
+                    setLocale("te")
                     recreate()
                 }
                 dialog.dismiss()
@@ -60,5 +48,40 @@ class LanguageAvtivity : AppCompatActivity() {
             alertBox.create().show()
 
         })
+
+
+        val nextBtn=findViewById<Button>(R.id.next_btn)
+        nextBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
     }
+
+    private fun loadLanguage(){
+        val sharedPrefrence = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        var lang = sharedPrefrence.getString("lang","")
+        setLocale(lang.toString())
+        if(lang!=""){
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun setLocale(lang: String){
+        var locale = Locale(lang)
+        Locale.setDefault(locale)
+        val congif = Configuration()
+        congif.locale = locale
+        baseContext.resources.updateConfiguration(congif,baseContext.resources.displayMetrics)
+
+        val sharedPrefrenceEditor = getSharedPreferences("Settings", Activity.MODE_PRIVATE).edit()
+        sharedPrefrenceEditor.putString("lang",lang)
+        sharedPrefrenceEditor.apply()
+
+
+    }
+
 }
+
